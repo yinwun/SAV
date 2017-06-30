a0296628d100be742756f12da744bbd57cd766ca
yinwun <liang_niki@hotmail.com>
2017-06-30 18:22:04 +0800

    company20170630

--- SAV/DialogAccount.h
+++ SAV/DialogAccount.h
@@ -2,6 +2,7 @@
 #include "Common.h"
 #include "SALib.h"
 
+
 // CDialogAccount dialog
 
 class CDialogAccount : public CDialogEx
@@ -27,12 +28,9 @@ protected:
 public:
 	virtual BOOL OnInitDialog();
 	CListCtrl m_listAct;
-	void BindInfoList();
+	
 	afx_msg void OnBnClickedbtnactlogin();
 	void OnSinglelogin();
-	void CallbackMsg(NOTIFYPARA *pNotifyPara);
+	
 };
 
-void Callback_func(WPARAM wParam, NOTIFYPARA *pNotifyPara);
-void Set_Callback(CALLBACK_Func func, WPARAM wParam);
-void Callback_Notify(NOTIFYPARA *pNotifyPara);
